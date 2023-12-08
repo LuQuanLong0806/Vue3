b75cb52fa0be2a5715d568e6f61252782fd924ca
鹿全龙 <luql@yeyoo.cn>
2023-12-08 17:16:08 +0800

    初始化dev分支

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <h1 class="p-40">Hello TailWindCSS!</h1>
+    <h1>初始化 dev分支</h1>
   </div>
 </template>
 
